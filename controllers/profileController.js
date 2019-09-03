0cae79596cb2bcfa5e468ec6135b443f0913f865
pmarques <pmarques@idealista.com>
2019-09-03 19:09:53 +0200

    Improvements login page and profile

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,5 +1,5 @@
 module.exports = {
     index : function(req, res){
-        res.render("profile.ejs", {userData: req.user}); 
+        res.render("profile.ejs", {userData: req.user, userSongs: "poo"}); 
     }
 }
