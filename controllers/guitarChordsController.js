f8f01e094c81f695450361d1534a6075def53b05
pedromaresteves <pedromaresteves@gmail.com>
2019-10-27 20:23:19 +0100

    Logged in Logic

--- controllers/guitarChordsController.js
+++ controllers/guitarChordsController.js
@@ -20,13 +20,13 @@ module.exports = {
       });
     },
     getAddSong : function(req,res){   
-      res.render("addSong.ejs", {userData: req.user, songData:{artist:req.params.artist}})
+      res.render("addOrEditSong.ejs", {userData: req.user, songData:{artist:req.params.artist}})
     },
     getEditSong : function(req,res){
       let artistRegex = new RegExp("^" + utils.escapeRegExp(req.params.artist) + "$", "gi");
       let titleRegex = new RegExp("^" + utils.escapeRegExp(req.params.title) + "$", "gi");
       SongModel.findOne({nTitle: titleRegex, nArtist: artistRegex}).then(result => {
-        res.render("addSong.ejs", {userData: req.user, songData : result})
+        res.render("addOrEditSong.ejs", {userData: req.user, songData : result})
       }).catch(err => {
         res.render("error.ejs", {userData: req.user, url: req.url, errorMessage: err.message});
       });
