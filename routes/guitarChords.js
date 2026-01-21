5eb694d42b67912b0e721e62982b1636d11c9266
Pedro Esteves <pedromaresteves@gmail.com>
2026-01-21 22:24:39 +0000

    Refactor routes

--- routes/guitarChords.js
+++ routes/guitarChords.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const guitarChordsController = require("../controllers/guitarChordsController");
-const bodyParser = require("body-parser");
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
+const guitarChordsController = require('../controllers/guitarChordsController');
 
-router.get("/", guitarChordsController.index);  
-router.get("/add-song/:artist?", guitarChordsController.getAddSong);
-router.get("/edit-song/:artist/:title", urlencodedParser, guitarChordsController.getEditSong);
-router.get("/:artist/", guitarChordsController.artistList);
-router.get("/:artist/:title", guitarChordsController.song);
+// Use express built-in urlencoded parser for routes that expect form data
+router.use(express.urlencoded({ extended: false }));
+
+router.get('/', guitarChordsController.index);
+router.get('/add-song/:artist?', guitarChordsController.getAddSong);
+router.get('/edit-song/:artist/:title', guitarChordsController.getEditSong);
+router.get('/:artist/', guitarChordsController.artistList);
+router.get('/:artist/:title', guitarChordsController.song);
 
 module.exports = router;
--- routes/profile.js
+++ routes/profile.js
@@ -1,15 +1,12 @@
-var express = require('express');
-var router = express.Router();
-var profileController = require("../controllers/profileController");
+const express = require('express');
+const router = express.Router();
+const profileController = require('../controllers/profileController');
 
-const authCheck = (req,res,next) => {
-    if(!req.user){
-        return res.redirect("/auth/login/")
-    }
+const authCheck = (req, res, next) => {
+    if (!req.user) return res.redirect('/auth/login/');
     return next();
-}
+};
 
-router.get("/", authCheck, profileController.index);  
-
+router.get('/', authCheck, profileController.index);
 
 module.exports = router;
